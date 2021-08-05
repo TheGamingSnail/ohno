64c9c9057c7ba7bb651e84a1a05a4c98f4a0d547
TheGamingSnail <87595446+TheGamingSnail@users.noreply.github.com>
2021-08-06 02:52:15 +0300

    added files

---
+++ src/Gamingsnail7410/ohno/Main.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace GamingSnail7410\ohno;
+
+use pocketmine\plugin\PluginBase;
+
+use pocketmine\event\player\PlayerJoinEvent;
+
+use pocketmine\Player;
+
+use pocketmine\Server;
+
+use pocketmine\event\Listener;
+
+class Main extends PluginBase implements Listener{
+
+public function onEnable(){
+  $this->getLogger()->info("hope this works!")
+ }
+
+public function onJoin(PlayerJoinEvent $event){
+  $player = $event->getPlayer();
+  $name = $player->getName();
+  
+  $this->getServer()->broadcastMessage("Hello $name, nice to see you!")
+ }
+
+}
